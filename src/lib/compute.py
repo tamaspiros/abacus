f7a4c0f18064ffe2fbed05d59181a0abe1b7ced6
tpiros1 <tamaspiros@yahoo.co.uk>
2019-01-11 08:29:01 +0100

    Add support for exponents

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -24,4 +24,7 @@ class Compute:
         for item in self.operands:
             product *= item
         print(product)
+    def exponent(self):
+        num_exponent = self.operands[0] ** self.operands[1]
+        print(num_exponent)
 
